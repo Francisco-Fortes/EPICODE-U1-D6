24f39b118364448328175b59e7fbe1c000cc31e2
Francisco-Fortes <fortesfco@gmail.com>
2022-10-04 16:58:56 +0200

    Initial commit

---
+++ first.js
@@ -0,0 +1,12 @@
+let firstName = "Francisco";
+let secondName = "Javier";
+let firstSurname = "Fortes";
+let secondSurname = "Carrillo";
+console.log(
+  "Hello, my name is",
+  firstName + " " + secondName + " " + firstSurname + " " + secondSurname
+);
+let myAge = 32;
+let currentYear = 2022;
+let birthYear = currentYear - myAge;
+console.log("I was born in", birthYear);
